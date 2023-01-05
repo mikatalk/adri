fec8164c681823432603b062b8142369b2b2a401
Mikatalk <mikatalk@users.noreply.github.com>
2023-01-05 08:27:30 -0800

    First commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
 import { defineConfig } from 'astro/config'
 import svelte from '@astrojs/svelte'
 import mdx from '@astrojs/mdx'
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astro-blog-template.netlify.app',
-  integrations: [mdx(), svelte()],
+  // site: 'https://astro-blog-template.netlify.app',
+  // integrations: [mdx(), svelte()],
+  site: 'https://mikatalk.github.io',
+  base: '/adri',
+  integrations: [mdx(), , svelte(), sitemap()],
+  output: 'static',
   markdown: {
     shikiConfig: {
       theme: 'nord',
